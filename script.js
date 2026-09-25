349f3d658372a15b0fcf1326a21197e722c4f0c9
ekdgkwlakfwk <infogressinc@gmail.com>
2026-09-25 18:35:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -123,7 +123,7 @@ function calculate() {
         <td><strong>${mat.name}</strong></td>
         <td>${totalArea.toLocaleString()}㎟</td>
         <td>${areaBaseCount}장<br><small>(${rawAreaCount.toFixed(1)}장 내림)</small></td>
-        <td><strong>${finalCount}장</strong><br><small>(${finalRawCount.toFixed(1)}장 내림)</small></td>
+        <td><strong>${finalCount}장</strong><br><small>(${finalRawCount.toFixed(1)}장 올림)</small></td>
         <td>
           <button class="detail-btn" onclick="toggleDetail('${detailId}')">상세보기</button>
           <div id="${detailId}" class="detail-box">${detailHTML}</div>
