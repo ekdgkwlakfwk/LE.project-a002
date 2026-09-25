d3b16815be64905e52b35570d1365c9618d161b7
ekdgkwlakfwk <infogressinc@gmail.com>
2026-09-25 18:24:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const matList = [
-  { name: "600size", size: 600, area: 360,000, lossRate: 0.065 },
+  { name: "600size", size: 600, area: 360000, lossRate: 0.065 },
   { name: "1200size", size: 1200, area: 1440000, lossRate: 0.11 },
   { name: "1300size", size: 1300, area: 1690000, lossRate: 0.11 }
 ];
